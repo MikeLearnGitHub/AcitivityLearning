b9fba414d1a77493fd38a8a126e896ef197c4390
mike2004_chen@hotmail.com <mike2004_chen@hotmail.com>
2021-02-28 15:57:35 -0700

    create toast message

--- app/src/main/java/com/example/acitivitylearning/MainActivity.kt
+++ app/src/main/java/com/example/acitivitylearning/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.acitivitylearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nextButton: Button = findViewById(R.id.btnNext)
+        nextButton.setOnClickListener {
+            Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show()
+        }
     }
 }
